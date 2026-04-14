f86f75764a067f71910877586012d0d0e85ff69e
Dmytro Kadar <dmytrokadar@gmail.com>
2026-04-14 17:53:30 +0200

    working player character

---
+++ godot/scenes/player.gd
@@ -0,0 +1,52 @@
+extends CharacterBody3D
+
+@onready var head = $Head
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var MOUSE_SENSITIVITY = 0.2
+@export var LERP_SPEED = 10.0
+
+var is_mouse_visible = false
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and not is_mouse_visible:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENSITIVITY))
+		head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENSITIVITY))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	if Input.is_action_just_pressed("unattach_cursor"):
+		if is_mouse_visible:
+			is_mouse_visible = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			is_mouse_visible = true
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forvard", "move_backwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
